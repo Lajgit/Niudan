2f686d9f54a23fe84b8e3690a57ca8e890d552b4
Lajgit <3243570032@qq.com>
2026-08-14 10:31:05 +0800

    移除旧球洞按键参数

--- Task/Inc/KeyTask.h
+++ Task/Inc/KeyTask.h
@@ -2,10 +2,8 @@
 #define __KEYTASK_H__
 
 #define KEY_DEBOUNCE_TIME 15
-#define HOLE_DEBOUNCE_TIME 7
 #define KEY_LONG_PRESS_TIME 800
 #define KEY_LONG_TRIGGER_FREQUENCY 10
-
 
 void KeyAll_Init(void);
 void Key_Task(void);
