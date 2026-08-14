410792bfec3fe5f507686eb99c166c87f9f13f1a
Lajgit <3243570032@qq.com>
2026-08-14 16:16:50 +0800

    修正剩余数量与出货超时上报协议

--- Task/Src/MesgTask.c
+++ Task/Src/MesgTask.c
@@ -29,7 +29,7 @@ void Mesg_Task(void)
     // 按键进入设置
     if (EventGroupCheckBits(&Mesg_event, MesgEvent_ButtonEnterSetting))
     {
-        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_SettingButton, 0x03, 0x01);
+        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_SettingButton, 0x03, KEY_EVENT_SHORT);
         EventGroupClearBits(&Mesg_event, MesgEvent_ButtonEnterSetting);
     }
     // 开锁
@@ -70,12 +70,34 @@ void Mesg_Task(void)
         Comm_SendMesg_FillData_withResend(&Tx1, Board_to_Android, t_CoinInput, 0x00, 0x00, &ResendList);
         EventGroupClearBits(&Mesg_event, MesgEvent_CoinInput);
     }
-    // 吐珠超时
-    if (EventGroupCheckBits(&Mesg_event, MesgEvent_HoolleOutputTimeout))
+
+    /*
+     * 中文注释：0x07出货超时使用ExpandCode区分通道：
+     * 0x00=扭蛋（Motor_Hoolle2），0x01=钢珠（Motor_Hoolle1）。
+     */
+    if (EventGroupCheckBits(&Mesg_event, MesgEvent_EggOutputTimeout))
     {
-        Comm_SendMesg_FillData_withResend(&Tx1, Board_to_Android, t_HoolleOutputTimeOut, (uint32_t)Motor_Hoolle2.Hoolle_num, 0x00, &ResendList);
-        EventGroupClearBits(&Mesg_event, MesgEvent_HoolleOutputTimeout);
+        Comm_SendMesg_FillData_withResend(
+            &Tx1,
+            Board_to_Android,
+            t_HoolleOutputTimeOut,
+            (uint32_t)Motor_Hoolle2.Hoolle_num,
+            HOOLLE_TYPE_EGG,
+            &ResendList);
+        EventGroupClearBits(&Mesg_event, MesgEvent_EggOutputTimeout);
     }
+    if (EventGroupCheckBits(&Mesg_event, MesgEvent_SteelBallOutputTimeout))
+    {
+        Comm_SendMesg_FillData_withResend(
+            &Tx1,
+            Board_to_Android,
+            t_HoolleOutputTimeOut,
+            (uint32_t)Motor_Hoolle1.Hoolle_num,
+            HOOLLE_TYPE_STEEL_BALL,
+            &ResendList);
+        EventGroupClearBits(&Mesg_event, MesgEvent_SteelBallOutputTimeout);
+    }
+
     // 吐卡超时
     if (EventGroupCheckBits(&Mesg_event, MesgEvent_CardOutputTimeout))
     {
@@ -83,24 +105,33 @@ void Mesg_Task(void)
         EventGroupClearBits(&Mesg_event, MesgEvent_CardOutputTimeout);
     }
 
-    // 每次吐卡后发送剩余卡片数：功能码0x05，补充位2为0x01
+    /*
+     * 中文注释：0x05统一表示剩余待出数量，ExpandCode固定为：
+     * 0x00=扭蛋，0x01=卡片，0x02=钢珠。
+     */
     if (EventGroupCheckBits(&Mesg_event, MesgEvent_CardOutputOnce))
     {
-        Comm_SendMesg_FillData(&Tx1,Board_to_Android,t_RemainingHoolle,(uint32_t)Card.Card_num,0x01);
-        EventGroupClearBits(&Mesg_event,MesgEvent_CardOutputOnce);
+        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_RemainingHoolle, (uint32_t)Card.Card_num, REMAIN_TYPE_CARD);
+        EventGroupClearBits(&Mesg_event, MesgEvent_CardOutputOnce);
+    }
+
+    if (EventGroupCheckBits(&Mesg_event, MesgEvent_RemainingEgg))
+    {
+        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_RemainingHoolle, (uint32_t)Motor_Hoolle2.Hoolle_num, REMAIN_TYPE_EGG);
+        EventGroupClearBits(&Mesg_event, MesgEvent_RemainingEgg);
+    }
+
+    if (EventGroupCheckBits(&Mesg_event, MesgEvent_RemainingSteelBall))
+    {
+        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_RemainingHoolle, (uint32_t)Motor_Hoolle1.Hoolle_num, REMAIN_TYPE_STEEL_BALL);
+        EventGroupClearBits(&Mesg_event, MesgEvent_RemainingSteelBall);
     }
 
     // 剩余卡片数为0时通知Unity关闭无卡提示弹窗：功能码0x11
     if (EventGroupCheckBits(&Mesg_event, MesgEvent_CardOutputFinish))
     {
-        Comm_SendMesg_FillData(&Tx1,Board_to_Android,t_ClearRemainMesg,0x00,0x00);
-        EventGroupClearBits(&Mesg_event,MesgEvent_CardOutputFinish);
-    }
-    // 剩余珠子数
-    if (EventGroupCheckBits(&Mesg_event, MesgEvent_RemainingHoolle) == true)
-    {
-        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_RemainingHoolle, (uint32_t)Motor_Hoolle2.Hoolle_num, 0x00);
-        EventGroupClearBits(&Mesg_event, MesgEvent_RemainingHoolle);
+        Comm_SendMesg_FillData(&Tx1, Board_to_Android, t_ClearRemainMesg, 0x00, 0x00);
+        EventGroupClearBits(&Mesg_event, MesgEvent_CardOutputFinish);
     }
     // 版本请求
     if (EventGroupCheckBits(&Mesg_event, MesgEvent_VersionRequest) == true)
