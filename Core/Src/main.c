c4715c0d847f528a924c6eb95d8447678f1f61f0
Lajgit <3243570032@qq.com>
2026-08-14 10:30:20 +0800

    按扭蛋机原理图精简启动外设

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,9 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "dma.h"
 #include "iwdg.h"
-#include "spi.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -91,20 +89,14 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_SPI1_Init();
   MX_TIM1_Init();
   MX_TIM2_Init();
-  MX_TIM3_Init();
-  MX_UART5_Init();
   MX_USART1_UART_Init();
   MX_TIM7_Init();
   MX_IWDG_Init();
-  MX_TIM5_Init();
-  MX_TIM10_Init();
-  MX_TIM11_Init();
   MX_USART3_UART_Init();
   /* USER CODE BEGIN 2 */
+  /* 中文注释：按当前扭蛋机原理图，仅初始化实际使用的外设；旧弹界灯效、SD、UART5相关外设不再启动。 */
   Main_Init();
   /* USER CODE END 2 */
 
