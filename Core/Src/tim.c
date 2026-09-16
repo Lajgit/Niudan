b95126ce9bf2c6a09c6726797dde5ca54d985949
Lajgit <3243570032@qq.com>
2026-09-16 09:56:35 +0800

    修正TIM1互补输出空闲状态宏

--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -82,7 +82,7 @@ void MX_TIM1_Init(void)
   sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
-  sConfigOC.OCNIdleState = TIM_OCIDLESTATE_RESET;
+  sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
   {
     Error_Handler();
