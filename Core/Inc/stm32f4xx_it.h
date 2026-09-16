13cdbac8c71d0fc5bb962e6cb089e5d02bc97550
Lajgit <3243570032@qq.com>
2026-09-16 10:22:58 +0800

    增加第二卡片机反馈中断声明

--- Core/Inc/stm32f4xx_it.h
+++ Core/Inc/stm32f4xx_it.h
@@ -62,6 +62,7 @@ void EXTI4_IRQHandler(void);
 void DMA1_Stream4_IRQHandler(void);
 void DMA1_Stream5_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
+void EXTI15_10_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
